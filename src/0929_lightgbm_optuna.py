e595e26d6e389084d23c2e659b04d3270c4c603f
y-toshiya-1048 <y-toshiya-1048@outlook.jp>
2019-09-29 16:50:19 +0900

    lightgbm training by optuna

--- src/0929_lightgbm_optuna.py
+++ src/0929_lightgbm_optuna.py
@@ -32,10 +32,9 @@ def main(args):
         drop_rate = trial.suggest_uniform('drop_rate', 0, 1.0)
         learning_rate = trial.suggest_uniform('learning_rate', 0, 1.0)
         subsample = trial.suggest_uniform('subsample', 0.8, 1.0)
-        num_leaves = trial.suggest_int('num_leaves', 5, 1000)
-        # num_boost_round = trial.suggest_int('num_boost_round', 10, 100000)
-        num_boost_round = trial.suggest_int('num_boost_round', 10, 100000)
-        min_data_in_leaf = trial.suggest_int('min_data_in_leaf', 10, 100000)
+        num_leaves = trial.suggest_int('num_leaves', 5, 10000)
+        num_boost_round = trial.suggest_int('num_boost_round', 10, 10000)
+        min_data_in_leaf = trial.suggest_int('min_data_in_leaf', 1, 1000)
         min_child_samples = trial.suggest_int('min_child_samples', 5, 500)
         min_child_weight = trial.suggest_int('min_child_weight', 5, 500)
 
@@ -45,30 +44,33 @@ def main(args):
             'objective' : 'regression',
             "learning_rate": learning_rate,
             "num_leaves": num_leaves,
-            "max_bin": 256,
+            # "max_bin": 256,
             "metrics": 'rmse',
-            # "drop_rate": drop_rate,
+            "drop_rate": drop_rate,
+            "num_boost_round": num_boost_round,
             # "is_unbalance": False,
-            # "max_drop": 50,
-            # "min_data": 1,
             # "min_child_samples": min_child_samples,
             # "min_child_weight": min_child_weight,
-            # "min_split_gain": 0,
-            # "min_data_in_leaf": min_data_in_leaf,
+            "min_split_gain": 0,
+            "min_data_in_leaf": min_data_in_leaf,
             # "subsample": subsample
+            "n_jobs": -1,
+            # "device_type": 'gpu',
+            'verbose': -1,
+            "seed":0
         }
                 
         # print('1')
         # mdl = lgb.train(params, lgb_train,num_boost_round=num_boost_round)
-        cv_results = lgb.cv(lgbm_params, lgb_train, nfold=10, num_boost_round=100)
+        cv_results = lgb.cv(lgbm_params, lgb_train, nfold=args.k_fold,stratified=False)
 
         # scores = cv_results['rmse']
         # print(cv_results)
         
         # print('4')
         # score = np.sqrt(abs(scores)).mean()
-        score = cv_results['rmse-mean']
-
+        # print(len(cv_results['rmse-mean']))
+        score = np.array(cv_results['rmse-mean']).mean()
 
         return score
 
@@ -87,59 +89,45 @@ def main(args):
     print()
 
 
-    # mdl = SVR(**study.best_params)
-    # X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-    # mdl.fit(X_train, y_train)
+    print('---------------------------------')    
+    print('predict test data')
+    print('---------------------------------')
+    print()    
 
-    # pred_train = mdl.predict(X_train)
-    # rmse_train = np.sqrt(mean_squared_error(y_train, pred_train))
-    # pred_test = mdl.predict(X_test)
-    # rmse_test = np.sqrt(mean_squared_error(y_test, pred_test))
+    params = study.best_params
 
-    # print('---------------------------------')
-    # print('train score :', rmse_train)
-    # print('test score :', rmse_test)
-    # print('---------------------------------')
-    # print()
+    mdl = lgb.train(params,lgb_train)
 
-    # print('---------------------------------')    
-    # print('predict test data')
-    # print('---------------------------------')
-    # print()    
+    test_df = pd.read_csv('~/Workspace/learning/signate/mynavi_2019/processed_data/test_v2.csv')
+    input_df = test_df.loc[:,['age','area','floor', 'structure', 'max_floor','room_num', 'L', 'D', 'K', 'S', 'north']]
 
-    # mdl = SVR(**study.best_params)
-    # mdl.fit(X, y)
+    y_pred = mdl.predict(input_df,num_iteration=mdl.best_iteration)
 
-    # test_df = pd.read_csv('~/Workspace/learning/signate/mynavi_2019/processed_data/test_v2.csv')
-    # input_df = test_df.loc[:,['age','area','floor', 'structure', 'max_floor','room_num', 'L', 'D', 'K', 'S', 'north']]
+    submit_file = pd.concat([test_df['id'],pd.Series(y_pred)],axis=1)
+    submit_file.to_csv('~/Workspace/learning/signate/mynavi_2019/submit/0929_submit.csv',header=False,index=False)
 
-    # y_pred = mdl.predict(input_df)
-
-    # submit_file = pd.concat([test_df['id'],pd.Series(y_pred)],axis=1)
-    # submit_file.to_csv('~/Workspace/learning/signate/mynavi_2019/submit/0928_submit_2.csv',header=False,index=False)
-
-    # print('Done!')
+    print('Done!')
     
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="training SVR by Optuna")
+    parser = argparse.ArgumentParser(description="training LightGBM by Optuna")
 
     parser.add_argument(
         '--n_trials',
         '-n',
         help ='training iteration',
         type=int,
-        default=10
+        default=100
     )
 
-    # parser.add_argument(
-    #     '--k_fold',
-    #     '-k',
-    #     help ='number of k-fold split',
-    #     type=int,
-    #     default=5
-    # )
+    parser.add_argument(
+        '--k_fold',
+        '-k',
+        help ='number of k-fold split',
+        type=int,
+        default=5
+    )
 
     args = parser.parse_args()
 
